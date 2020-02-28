f4774cc07f0e843605d39d6baebd21487f363a2d
mateo m <Mateo.MAHMUTOVIC@amplexor.com>
2020-02-28 16:36:38 +0100

    3rd commit

--- app.js
+++ app.js
@@ -1,35 +1,53 @@
-//IIFE 
+ 
+/**
+ * IIFE 
+ * 1) Setting up first event listeners
+ * 
+ */
+ // BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a;
-    }
-
-    return {
-        publicTest: function(b) {
-            return(add(b));
-        }
-    }
+ 
 })();
 
 
+
+
+// UI CONTROLLER
 var UIController = (function() {
 
-    //some code
+    
 
 })();
 
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
+        // 1. get the field input data
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 2. add the item to the budget controller
+
+        // 3. add the new item to the UI
+
+        // 4. calculate the budget
+
+        // 5. display the budget on the UI
+        console.log('it works');
     }
+
+
+    // using HTML CLASS selector
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem()
+        }
+
+    });
 
 })(budgetController, UIController);
