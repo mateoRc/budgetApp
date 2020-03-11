5d1eb132e96e44d9662116d818a2172af8754773
YoDaddy <mmateo19992@gmail.com>
2020-03-11 20:53:48 +0100

    final

--- app.js
+++ app.js
@@ -23,6 +23,7 @@
  * 17) Improve input field UX
  * 
  * TODO: finishing touches
+ * 18) Change event
  * 
  */
 
@@ -246,6 +247,12 @@ var UIController = (function() {
         return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
+
     return {
 
         getInput: function() {
@@ -324,12 +331,6 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMStrings.expensesPercLabel); //returns a node-list 
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
                 if (percentages[index] > 0) {
                     current.textContent = percentages[index] + '%';
@@ -349,7 +350,22 @@ var UIController = (function() {
             year = now.getFullYear();
             
             document.querySelector(DOMStrings.dateLabel).textContent = months[month] + ' ' + year;
-        }, 
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMStrings.inputType + ',' +
+                DOMStrings.inputDescription + ',' +
+                DOMStrings.inputValue);
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
+            document.querySelector(DOMStrings.inputBtn).classList.toggle('red');
+
+        },
 
         getDOMStrings: function() {
             return DOMStrings;
@@ -385,6 +401,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
        document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
 
+       document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
     };
 
     var updatePercentages = function() {
